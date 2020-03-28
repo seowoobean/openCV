c74b15d91f4bd5c873963a8534df3b2e3f712039
seowoobean <seowoobean@gmail.com>
2020-03-28 18:21:44 +0900

    Video read ex [video_read.py]

---
+++ video_read.py
@@ -0,0 +1,17 @@
+import cv2
+
+video_file = "C:/Download/video.mp4"
+
+cap = cv2.VideoCapture(video_file)
+if cap.isOpened():
+    while True:
+        ret, img = cap.read()
+        if ret:
+            cv2.imshow(video_file, img)
+            cv2.waitKey(25)
+        else:
+            break
+else:
+    print("can't open video.")
+cap.release()
+cv2.destroyAllWindows
